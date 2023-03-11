128b70e9937bb62409ac2dddf109ea25811ba5d4
Masho Ado <mashoado@gmail.com>
2023-03-11 12:33:12 +0300

    add more api response

--- config/api-response.ts
+++ config/api-response.ts
@@ -2,4 +2,7 @@ export const SUCCESS_MESSAGE = {
   USER_LOGGED_IN: "User_Successfully_Logged_In",
   FAILED_LOGGED_IN: "User_Failed_To_Logged_In",
   PRODUCT_SUCCESSFULLY_CREATED: "Product_Successfully_Created",
+  FAILED_TO_CREATE_PRODUCT: "Failed_To_Create_Product",
+  USER_REGISTERED_SUCCESSFULLY: "User_Registered_Successfully",
+  USER_REGISTRATION_FAILED: "User_Registration_Failed",
 };
