37efd39fb2c7caf10c19438d713a318ea9c2bbed
Masho Ado <mashoado@gmail.com>
2023-03-11 12:39:04 +0300

    add auth middlewares to appropriate routes

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import productsRoutes from "../controllers/product";
 import usersRoutes from "../controllers/user";
+import AuthMiddleware from "../middleware/auth";
 
 const router = Router();
 
-router.use("/products", productsRoutes);
 router.use("/users", usersRoutes);
+router.use(AuthMiddleware);
+router.use("/products", productsRoutes);
 
 export default router;
